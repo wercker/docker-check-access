2a01ad243b4a5c282dfbea649ba0922f2885e73c
Mike Gilbode <mike.gilbode@oracle.com>
2018-04-04 09:52:30 -0400

    Upgrade azure client.

--- azure.go
+++ azure.go
@@ -1,9 +1,12 @@
 package auth
 
 import (
+	"context"
 	"fmt"
-
-	"github.com/Azure/azure-sdk-for-go/arm/containerregistry"
+	"github.com/Azure/azure-sdk-for-go/services/authorization/mgmt/2015-07-01/authorization"
+	"github.com/Azure/azure-sdk-for-go/services/containerregistry/mgmt/2017-10-01/containerregistry"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure"
 )
 
@@ -11,7 +14,10 @@ type Azure struct {
 	resourceGroupName string
 	registryName      string
 	registryURL       string
+	subscriptionID    string
+	authorizer        autorest.Authorizer
 	regClient         containerregistry.RegistriesClient
+	roleClient        authorization.RoleAssignmentsClient
 	dockerUsername    string
 	dockerPassword    string
 }
@@ -23,59 +29,63 @@ func NewAzure(clientID, clientSecret, subscriptionID, tenantID, resourceGroupNam
 	if err != nil {
 		return nil, err
 	}
-	regClient := containerregistry.NewRegistriesClient(subscriptionID)
-	regClient.Authorizer = spt
+	authorizer := autorest.NewBearerAuthorizer(spt)
 	return &Azure{
 		resourceGroupName: resourceGroupName,
 		registryName:      registryName,
 		registryURL:       registryURL,
-		regClient:         regClient,
+		subscriptionID:    subscriptionID,
+		authorizer:        authorizer,
+		dockerUsername:    clientID,
+		dockerPassword:    clientSecret,
 	}, nil
 }
 
 func (a *Azure) CheckAccess(Repository string, scope Scope) (bool, error) {
-	res, err := a.regClient.GetCredentials(a.resourceGroupName, a.registryName)
+	regClient := containerregistry.NewRegistriesClient(a.subscriptionID)
+	regClient.Authorizer = a.authorizer
+	registry, err := regClient.Get(context.Background(), a.resourceGroupName, a.registryName)
 	if err != nil {
 		return false, err
 	}
-	a.dockerUsername = *(res.Username)
-	a.dockerPassword = *(res.Password)
-	return true, nil
+
+	roleClient := authorization.NewRoleAssignmentsClient(a.subscriptionID)
+	roleClient.Authorizer = a.authorizer
+
+	rolesIt, err := roleClient.ListForResourceComplete(context.Background(), a.resourceGroupName, "Microsoft.ContainerRegistry", *(registry.ID), *(registry.Type), *(registry.Name), "atScope()")
+	if err != nil {
+		return false, err
+	}
+
+	canAccess := false
+	for role := rolesIt.Value(); rolesIt.NotDone(); rolesIt.Next() {
+		if scope == Pull && (*(role.Name) == "Reader" || *(role.Name) == "Contributor") {
+			canAccess = true
+			break
+		} else if scope == Push && *(role.Name) == "Contributor" {
+			canAccess = true
+			break
+		}
+	}
+
+	return canAccess, nil
 }
 
 func (a *Azure) Password() string {
-	var password string
-	if a.dockerPassword == "" {
-		_, err := a.CheckAccess("", Push)
-		if err == nil {
-			password = a.dockerPassword
-		}
-	} else {
-		password = a.dockerPassword
-	}
-	return password
+	return a.dockerPassword
 }
 func (a *Azure) Username() string {
-	var username string
-	if a.dockerUsername == "" {
-		_, err := a.CheckAccess("", Push)
-		if err == nil {
-			username = a.dockerUsername
-		}
-	} else {
-		username = a.dockerUsername
-	}
-	return username
+	return a.dockerUsername
 }
 
 func (a *Azure) Repository(repository string) string {
 	return fmt.Sprintf("%s/%s", a.registryURL, repository)
 }
 
-func newServicePrincipalTokenFromCredentials(clientID, clientSecret, tenantID, scope string) (*azure.ServicePrincipalToken, error) {
-	oauthConfig, err := azure.PublicCloud.OAuthConfigForTenant(tenantID)
+func newServicePrincipalTokenFromCredentials(clientID, clientSecret, tenantID, scope string) (*adal.ServicePrincipalToken, error) {
+	oauthConfig, err := adal.NewOAuthConfig(azure.PublicCloud.ActiveDirectoryEndpoint, tenantID)
 	if err != nil {
 		return nil, err
 	}
-	return azure.NewServicePrincipalToken(*oauthConfig, clientID, clientSecret, scope)
+	return adal.NewServicePrincipalToken(*oauthConfig, clientID, clientSecret, scope)
 }
